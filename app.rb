98484fa65f9dc9770b3084b436b61ba192501cb1
Mynssem <rafaelmynssem@id.uff.br>
2019-01-18 01:12:48 -0200

    models do projeto

--- app.rb
+++ app.rb
@@ -5,9 +5,12 @@ require 'sinatra'
 require 'sinatra/activerecord'
   
 require './config/database'
+
+# Load Models - o comando Dir serve para carregar arquivos
+Dir["./app/models/*.rb"].each {|file| require file }
   
 class App < Sinatra::Base
   get '/' do
-    'Hello world!'
+    'Hello world!!!!'
   end
 end
